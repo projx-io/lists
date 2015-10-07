9a74ce79f2dc76cbb6cd60f784a881c86d2d8d00
Terrence Howard <chemisus@gmail.com>
2015-10-07 11:03:38 -0400

    added merge reduction

--- src/Reductions.php
+++ src/Reductions.php
@@ -17,4 +17,11 @@ abstract class Reductions
             return $current * (is_callable($map) ? call_user_func($map, $value, $key) : $value);
         };
     }
+
+    public static function merge(callable $map = null)
+    {
+        return function ($current, $value, $key) use ($map) {
+            return array_merge($current, (is_callable($map) ? call_user_func($map, $value, $key) : $value));
+        };
+    }
 }
