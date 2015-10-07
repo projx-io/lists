7609f94108f5eb7114ebbbd77d459420d5f64004
Terrence Howard <chemisus@gmail.com>
2015-10-07 12:45:00 -0400

    making parameter name more accurate

--- src/Reductions.php
+++ src/Reductions.php
@@ -10,14 +10,14 @@ abstract class Reductions
      * Sums an array.
      *
      * @param callable|null $map
-     * @param boolean $ignore_initial
+     * @param boolean $ignore_initial_null
      * @return Closure
      */
-    public static function sum(callable $map = null, $ignore_initial = true)
+    public static function sum(callable $map = null, $ignore_initial_null = true)
     {
         $count = 0;
-        return function ($current, $value, $key) use ($map, &$count, $ignore_initial) {
-            $current = !$count++ && $ignore_initial && $current === null ? 0 : $current;
+        return function ($current, $value, $key) use ($map, &$count, $ignore_initial_null) {
+            $current = !$count++ && $ignore_initial_null && $current === null ? 0 : $current;
             return $current + (is_callable($map) ? call_user_func($map, $value, $key) : $value);
         };
     }
@@ -26,14 +26,14 @@ abstract class Reductions
      * Products an array.
      *
      * @param callable|null $map
-     * @param bool $ignore_initial
+     * @param bool $ignore_initial_null
      * @return Closure
      */
-    public static function product(callable $map = null, $ignore_initial = true)
+    public static function product(callable $map = null, $ignore_initial_null = true)
     {
         $count = 0;
-        return function ($current, $value, $key) use ($map, &$count, $ignore_initial) {
-            $current = !$count++ && $ignore_initial && $current === null ? 1 : $current;
+        return function ($current, $value, $key) use ($map, &$count, $ignore_initial_null) {
+            $current = !$count++ && $ignore_initial_null && $current === null ? 1 : $current;
             return $current * (is_callable($map) ? call_user_func($map, $value, $key) : $value);
         };
     }
@@ -44,11 +44,11 @@ abstract class Reductions
      * @param callable|null $map
      * @return Closure
      */
-    public static function merge(callable $map = null, $ignore_initial = true)
+    public static function merge(callable $map = null, $ignore_initial_null = true)
     {
         $count = 0;
-        return function ($current, $value, $key) use ($map, &$count, $ignore_initial) {
-            $current = !$count++ && $ignore_initial && $current === null ? [] : $current;
+        return function ($current, $value, $key) use ($map, &$count, $ignore_initial_null) {
+            $current = !$count++ && $ignore_initial_null && $current === null ? [] : $current;
             return array_merge($current, (is_callable($map) ? call_user_func($map, $value, $key) : $value));
         };
     }
@@ -58,14 +58,14 @@ abstract class Reductions
      *
      * @param string $glue
      * @param callable|null $map
-     * @param bool $ignore_initial
+     * @param bool $ignore_initial_null
      * @return Closure
      */
-    public static function implode($glue = '', callable $map = null, $ignore_initial = true)
+    public static function implode($glue = '', callable $map = null, $ignore_initial_null = true)
     {
         $count = 0;
-        return function ($current, $value, $key) use ($map, $glue, &$count, $ignore_initial) {
-            $current = !$count++ && $ignore_initial && $current === null ? '' : $current . $glue;
+        return function ($current, $value, $key) use ($map, $glue, &$count, $ignore_initial_null) {
+            $current = !$count++ && $ignore_initial_null && $current === null ? '' : $current . $glue;
             return $current . (is_callable($map) ? call_user_func($map, $value, $key) : $value);
         };
     }
@@ -74,14 +74,14 @@ abstract class Reductions
      * Performs a logical and on each item in an array.
      *
      * @param callable|null $map
-     * @param bool $ignore_initial
+     * @param bool $ignore_initial_null
      * @return Closure
      */
-    public static function ands(callable $map = null, $ignore_initial = true)
+    public static function ands(callable $map = null, $ignore_initial_null = true)
     {
         $count = 0;
-        return function ($current, $value, $key) use ($map, &$count, $ignore_initial) {
-            $current = !$count++ && $ignore_initial && $current === null ? true : $current;
+        return function ($current, $value, $key) use ($map, &$count, $ignore_initial_null) {
+            $current = !$count++ && $ignore_initial_null && $current === null ? true : $current;
             return $current && (is_callable($map) ? call_user_func($map, $value, $key) : $value);
         };
     }
@@ -90,14 +90,14 @@ abstract class Reductions
      * Performs a logical or on each item in an array.
      *
      * @param callable|null $map
-     * @param bool $ignore_initial
+     * @param bool $ignore_initial_null
      * @return Closure
      */
-    public static function ors(callable $map = null, $ignore_initial = true)
+    public static function ors(callable $map = null, $ignore_initial_null = true)
     {
         $count = 0;
-        return function ($current, $value, $key) use ($map, &$count, $ignore_initial) {
-            $current = !$count++ && $ignore_initial && $current === null ? false : $current;
+        return function ($current, $value, $key) use ($map, &$count, $ignore_initial_null) {
+            $current = !$count++ && $ignore_initial_null && $current === null ? false : $current;
             return $current || (is_callable($map) ? call_user_func($map, $value, $key) : $value);
         };
     }
@@ -106,14 +106,14 @@ abstract class Reductions
      * Finds the max of an array.
      *
      * @param callable|null $map
-     * @param bool $ignore_initial
+     * @param bool $ignore_initial_null
      * @return Closure
      */
-    public static function max(callable $map = null, $ignore_initial = true)
+    public static function max(callable $map = null, $ignore_initial_null = true)
     {
         $count = 0;
-        return function ($current, $value, $key) use ($map, &$count, $ignore_initial) {
-            $current = !$count++ && $ignore_initial && $current === null ? $value : $current;
+        return function ($current, $value, $key) use ($map, &$count, $ignore_initial_null) {
+            $current = !$count++ && $ignore_initial_null && $current === null ? $value : $current;
             return max($current, (is_callable($map) ? call_user_func($map, $value, $key) : $value));
         };
     }
@@ -122,14 +122,14 @@ abstract class Reductions
      * Finds the min of an array.
      *
      * @param callable|null $map
-     * @param bool $ignore_initial
+     * @param bool $ignore_initial_null
      * @return Closure
      */
-    public static function min(callable $map = null, $ignore_initial = true)
+    public static function min(callable $map = null, $ignore_initial_null = true)
     {
         $count = 0;
-        return function ($current, $value, $key) use ($map, &$count, $ignore_initial) {
-            $current = !$count++ && $ignore_initial && $current === null ? $value : $current;
+        return function ($current, $value, $key) use ($map, &$count, $ignore_initial_null) {
+            $current = !$count++ && $ignore_initial_null && $current === null ? $value : $current;
             return min($current, (is_callable($map) ? call_user_func($map, $value, $key) : $value));
         };
     }
@@ -138,14 +138,14 @@ abstract class Reductions
      * Finds the average of an array.
      *
      * @param callable|null $map
-     * @param bool $ignore_initial
+     * @param bool $ignore_initial_null
      * @return Closure
      */
-    public static function average(callable $map = null, $ignore_initial = true)
+    public static function average(callable $map = null, $ignore_initial_null = true)
     {
         $count = 0;
-        return function ($current, $value, $key) use ($map, &$count, $ignore_initial) {
-            $current = !$count++ && $ignore_initial && $current === null ? [] : $current;
+        return function ($current, $value, $key) use ($map, &$count, $ignore_initial_null) {
+            $current = !$count++ && $ignore_initial_null && $current === null ? [] : $current;
             return ($current * ($count - 1) + (is_callable($map) ? call_user_func($map, $value, $key) : $value)) / $count;
         };
     }
